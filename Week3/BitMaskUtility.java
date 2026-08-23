4efbe6e636416ec1359bd80580f2dcdc05cbb5be
Chehak <chehakarora08@gmail.com>
2026-08-23 17:41:51 +0530

    Create BitMaskUtility.java

---
+++ Week3/BitMaskUtility.java
@@ -0,0 +1,35 @@
+public class BitMaskUtility
+{
+    static boolean isBitSet(int number,int position)
+    {
+        return (number&(1<<position))!=0;
+    }
+
+    static int setBit(int number,int position)
+    {
+        return number|(1<<position);
+    }
+
+    static int clearBit(int number,int position)
+    {
+        return number&~(1<<position);
+    }
+
+    static int toggleBit(int number,int position)
+    {
+        return number^(1<<position);
+    }
+
+    static String toBinary32(int number)
+    {
+        String binary="";
+        for(int i=31;i>=0;i--)
+        {
+            if((number&(1<<i))!=0)
+                binary+="1";
+            else
+                binary+="0";
+        }
+        return binary;
+    }
+}
