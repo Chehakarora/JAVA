247f77b71505064a7e744ee29234bf6fbe8a8817
Chehak <chehakarora08@gmail.com>
2026-09-20 18:07:40 +0530

    Create Person.java

---
+++ Week7/POLYMORPHISM AND SAFE CASTING/Person.java
@@ -0,0 +1,16 @@
+public class Person {
+
+    protected String name;
+    protected int age;
+
+    void displayName(){
+        System.out.println("Function 1 from Person");
+        System.out.println("Name: "+ name);
+    }
+    void displayAge() {
+        System.out.println("Function 2 from Person");
+        System.out.println("Age: "+age);
+    }
+
+}
+
